1dd221d7cab58d72300896c05745623d5d5dac2d
estapitta <efspitta@gmail.com>
2021-01-18 13:10:40 -0600

    data fetch api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {fetchContactList} from './services.js'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={fetchContactList}>HELLO</button>
         <a
           className="App-link"
           href="https://reactjs.org"
