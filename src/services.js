cd3acf53dcf707567142d3a6fa3300f6704cbe68
estapitta <efspitta@gmail.com>
2021-01-18 13:43:51 -0600

    Removed extra fetch dependency

--- src/services.js
+++ src/services.js
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch'
 const corsanywhere = "https://cors-anywhere.herokuapp.com/"
 const baseUrl = "https://sahmed93846.api-us1.com/api/3"
 const apiKey = "bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0"
@@ -6,15 +5,15 @@ const contactEndPoint = '/contacts'
 const tagsEndPoint = '/tags'
 
 export const fetchContactList = async() => {
-  try{
-const response = await fetch(corsanywhere + baseUrl + contactEndPoint + "?include=deals,geoIps.geoAddress,tags,contactTags.tag", {
-  headers:{
-    "Api-Token": apiKey
-  }
-})
-const data = await response.json()
-console.log(data)
-return data
+  try {
+    const response = await fetch(corsanywhere + baseUrl + contactEndPoint + "?include=deals,geoIps.geoAddress,tags,contactTags.tag", {
+      headers:{
+        "Api-Token": apiKey
+      }
+    })
+    const data = await response.json()
+    console.log(data)
+    return data
   } catch(error) {
     console.log(error)
   }
