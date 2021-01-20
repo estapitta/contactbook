6973f3f533d99430a7fb99e5dd5fb72fcbc134d8
estapitta <efspitta@gmail.com>
2021-01-19 20:21:01 -0600

    Normalized data to use for UI

--- src/services.js
+++ src/services.js
@@ -6,19 +6,69 @@ const tagsEndPoint = '/tags'
 
 export const fetchContactList = async() => {
   try {
+    console.log("Request")
     const response = await fetch(corsanywhere + baseUrl + contactEndPoint + "?include=deals,geoIps.geoAddress,tags,contactTags.tag", {
       headers:{
         "Api-Token": apiKey
       }
     })
     const data = await response.json()
-    console.log(data)
-    return data
+    console.log(normalizeData(data))
+
+    return normalizeData(data)
   } catch(error) {
     console.log(error)
   }
 }
 
+const normalizeData = (obj) => {
+  const contacts = obj.contacts.map((contact) => {
+    contact.fullName = contact.firstName + " " + contact.lastName 
+    contact.dealCount = contact.deals.length
+    let currency = ""
+    contact.valueTotal = contact.deals.reduce((total, dealId) => {
+      let foundDeal = obj.deals.find((deal) => deal.id === dealId)
+      if(foundDeal){
+        //Assuming every deal under one contact is of the same currency
+        currency = foundDeal.currency
+        return total + Number(foundDeal.value)
+      } else {
+        return total
+      }
+    }, 0)
+    contact.valueCurrency = currency
+
+    contact.geoIps.forEach((geoIpId) => {
+      const foundGeoIp = obj.geoIps.find((ip) => ip.id === geoIpId)
+      if(foundGeoIp) {
+        const geoAddrId = foundGeoIp.geoaddrid
+        const foundGeoAddr = obj.geoAddresses.find((addr) => addr.id === geoAddrId)
+        if(foundGeoAddr) {
+          contact.location = foundGeoAddr.city + ", " + foundGeoAddr.state + ", " + foundGeoAddr.country
+          // return here to end forEach since we found our location
+          return
+        }
+      }
+    })
+
+    const tags = obj.contactTags.reduce((acc, contactTag) => {
+      if(contactTag.contact === contact.id) {
+        const foundTag = obj.tags.find((tag) => tag.id === contactTag.tag)
+        if(foundTag) {
+          acc.push(foundTag.tag)
+        }
+      }
+
+      return acc
+    }, [])
+
+    contact.tags = tags.join(", ")
+
+    return contact
+  })
+
+  return contacts
+}
 // const fetchAllTags = async () => {
 //   try{
 //     const response = await fetch(corsanywhere + baseUrl + tagsEndPoint, {
